712e6b7802d95ff9aa32346b1010d6520cfa1796
nevalhr_macairm4 <phneval@gmail.com>
2026-01-02 02:47:36 +0700

    update data.jsin

--- blog/tulisUlang/tulisUlang.go
+++ blog/tulisUlang/tulisUlang.go
@@ -1,37 +1,10 @@
-// package tulisUlang
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"os"
-// )
-
-// type blogPost struct {
-// 	ID int `json:"id"`
-// }
-
-// func getPost() []blogPost {
-
-// 	fileBytes, err := os.ReadFile("data/data.json")
-
-// 	if err != nil {
-// 		fmt.Println("file cannot be read ")
-// 		return []blogPost{}
-// 	}
-
-// 	var posts []blogPost
-// 	err = json.Unmarshal(fileBytes, &posts)
-// 	if err != nil{
-// 		fmt.Println("failed to load file")
-// 		return []blogPost{}
-// 	}
-
-// 	return posts
-// }
-
 package tulisulang
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type blogpost struct {
 	ID int `json:"id"`
@@ -41,7 +14,15 @@ func getpost() []blogpost {
 	fileBytes, err := os.ReadFile("data/data.json")
 
 	if err != nil {
-		fmt.println
+		fmt.Println("failed read file")
 	}
+
+	var posts []blogpost
+	err = json.Unmarshal(fileBytes, &posts)
+	if err != nil {
+		fmt.Println("failed to load file")
+	}
+
+	return posts
 
 }
