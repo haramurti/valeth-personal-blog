2ebbccee470c249ce9caaa9edb49d79e831d4560
nevalhr_macairm4 <phneval@gmail.com>
2026-01-03 01:53:06 +0700

    update tulis ulang.go

--- blog/tulisUlang/tulisUlang.go
+++ blog/tulisUlang/tulisUlang.go
@@ -1,11 +1,16 @@
 package tulisulang
 
+//fully sendiri
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"net/http"
 	"os"
+	"sort"
 )
 
+// fully sendiri
 type blogpost struct {
 	ID int `json:"id"`
 }
@@ -24,5 +29,27 @@ func getpost() []blogpost {
 	}
 
 	return posts
+
+}
+
+//fully sendiri coy
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+
+	posts := getpost()
+
+	sort.Slice(posts, func(i, j int) bool {
+		return posts[i].ID > posts[j].ID
+	})
+
+	tmpl, err := template.ParseFiles("views/index.html")
+	if err != nil {
+		fmt.Println("cannot parse file html")
+	}
+
+	err = tmpl.Execute(w, posts)
+	if err != nil {
+		fmt.Println("cannot load file html.")
+	}
 
 }
